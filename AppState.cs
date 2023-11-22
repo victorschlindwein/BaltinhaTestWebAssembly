cd762ee44b832fc65e2cd1a29659d60a2fc9d37e
dizzysch <victorwilbert@gmail.com>
2023-11-22 17:36:32 -0300

    Adicionar arquivos de projeto.

---
+++ AppState.cs
@@ -0,0 +1,7 @@
+﻿namespace BaltinhaTest
+{
+    public static class AppState
+    {
+        public static string LoggedUser { get; set; } = String.Empty;
+    }
+}
---
+++ Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace BaltinhaTest
+{
+    public static class Configuration
+    {
+        public static string ApiUrl { get; set; } = "http://localhost:5095";
+    }
+}
