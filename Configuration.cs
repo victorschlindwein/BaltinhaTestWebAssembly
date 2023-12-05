b7f8b0f6e08ccb8470aede34d4816a946e5af49f
dizzysch <victorwilbert@gmail.com>
2023-12-05 12:41:00 -0300

    feat: implementa front-end pra busca de dados no projeto api-ibge

--- Configuration.cs
+++ Configuration.cs
@@ -3,5 +3,7 @@
     public static class Configuration
     {
         public static string ApiUrl { get; set; } = "http://localhost:5095";
+        public static string ApiIbgeCities { get; set; } = "http://localhost:5071/ibge/GetByCity";
+        public static string ApiIbgeState { get; set; } = "http://localhost:5071/ibge/GetByState";
     }
 }
